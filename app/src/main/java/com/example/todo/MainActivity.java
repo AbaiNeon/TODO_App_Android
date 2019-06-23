39ec80711f2459ff29369dcfe75dbd36d58f3c3d
abai <abai2010@bk.ru>
2019-06-23 14:02:10 +0600

    добавил ListView  с контекстным меню

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.todo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+//запись и чтение из БД
+//   добавь ListView с контекстн меню
 
 public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
+    ListView listView;
+    EditText editText;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        listView = (ListView) findViewById(R.id.listViewID);
+        list = new ArrayList<String>();
+
+        list.add("Point1");
+        list.add("Point2");
+        list.add("Point3");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        registerForContextMenu(listView);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0,1,0,"Edit");
+        menu.add(0,2,0,"Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        switch (item.getItemId()){
+            case 1:
+                Toast.makeText(MainActivity.this, "Edited", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+        return super.onContextItemSelected(item);
+
     }
 }
